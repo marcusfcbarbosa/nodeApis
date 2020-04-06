6e7628b5eb9b29047d591696c74e801e5983bcbd
Your Name <you@example.com>
2020-04-06 16:20:14 -0300

    Fazendo tratativa de erro

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use('/',route);
 
 //falando para o servidor ficar ouvindo a porta 3000
 server.listen(port);
+server.on('error',onError);
 
 console.log("API rodando na porta : "+port);
 
@@ -46,3 +47,24 @@ function normalizedPort(val){
     }
     return false;
 }
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+    
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+  
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+  
+      default:
+        throw error;
+    }
+  }
