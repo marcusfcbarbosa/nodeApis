655bdfe7241b4f63befc0b49f7fac99547ca1508
Your Name <you@example.com>
2020-04-07 15:18:02 -0300

    Instalando Body-parser para converter o corpo das requisicoes em formato json

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 'use strict'
 const express = require('express');
+const bodyParser = require('body-parser');
+
+
 const app = express();
 const router = express.Router();
 //criando a primeira rota
@@ -10,7 +13,17 @@ const route = router.get('/',(req,res,next)=>{
     });
 });
 
+//rota de criação
+const create = router.post('/',(req,res,next)=> {
+    res.status(201).send(req.body);
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.use('/',route);
+app.use('/products',create);
+
 
 //exportando a aplicação
 module.exports = app;
