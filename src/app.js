d611c0c713824eed5ac1b5d88eb233e0e916eaac
Your Name <you@example.com>
2020-04-07 15:38:57 -0300

    Criando CURD basico

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
 //criando a primeira rota
 const route = router.get('/',(req,res,next)=>{
     res.status(200).send({
@@ -18,11 +19,31 @@ const create = router.post('/',(req,res,next)=> {
     res.status(201).send(req.body);
 });
 
+//rota de edição
+const put = router.put('/:id',(req,res,next)=> {
+    const id = req.params.id;
+    res.status(201).send({
+        id:id,
+        item:req.body
+    });
+});
+
+const _delete = router.delete('/:id',(req,res,next)=> {
+    const id = req.params.id;
+    res.status(201).send({
+        id:id,
+        item:req.body
+    });
+});
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+
 app.use('/',route);
 app.use('/products',create);
+app.use('/products',put);
 
 
 //exportando a aplicação
